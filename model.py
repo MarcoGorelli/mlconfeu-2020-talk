be6dd1791adbd17e6b30bf2544db02d6501177ca
Marco Gorelli <m.e.gorelli@gmail.com>
2020-10-17 14:42:28 +0100

    lint model

--- model.py
+++ model.py
@@ -1,31 +1,31 @@
 import pandas as pd
-from sklearn.metrics import mean_squared_error as mse
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error as mse
 
 dataframes = {i: pd.read_csv(f"data/dataset_{i}.csv") for i in range(4)}
 
 for i in range(4):
-    x_mean = dataframes[i]['x'].mean()
+    x_mean = dataframes[i]["x"].mean()
     print(f"Dataset {i}: {x_mean=:.1f}")
 print()
 
 for i in range(4):
-    y_mean = dataframes[i]['y'].mean()
+    y_mean = dataframes[i]["y"].mean()
     print(f"Dataset {i}: {y_mean=:.1f}")
 print()
 
 for i in range(4):
-    x_std = dataframes[i]['x'].std()
+    x_std = dataframes[i]["x"].std()
     print(f"Dataset {i}: {x_std=:.1f}")
 print()
 
 for i in range(4):
-    y_std = dataframes[i]['y'].std()
+    y_std = dataframes[i]["y"].std()
     print(f"Dataset {i}: {y_std=:.1f}")
 print()
 
 for i in range(4):
-    corr_x_y = dataframes[i].corr().loc['x', 'y']
+    corr_x_y = dataframes[i].corr().loc["x", "y"]
     print(f"Dataset {i}: {corr_x_y=:.1f}")
 print()
 
